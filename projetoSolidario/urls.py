bbebc7eac60746c8230164c5c7d0b507a58d3119
Maiko Deizepi <maikodeizepi@hotmail.com>
2024-09-20 13:20:17 -0300

    V.1.0.5.1 alteracao botao new evento

--- projetoSolidario/urls.py
+++ projetoSolidario/urls.py
@@ -83,7 +83,7 @@ urlpatterns = [
     path("projetosolidario/perfil/", views.tela_perfil, name="perfil"),
     # TELACALENDARIO
     path("projetosolidario/calendario/", views.CalendarView.as_view(), name="calendar"),
-    path("projetosolidario/evento/novo/", views.EventView.as_view(), name="event_new"),
+    # path("projetosolidario/evento/novo/", views.EventView.as_view(), name="event_new"),
     path(
         "projetosolidario/evento/edit/<int:event_id>/",
         views.EventView.as_view(),
